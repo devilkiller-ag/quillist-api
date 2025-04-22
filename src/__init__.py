cdf5d9419cf16c51e575889ac6ec94be618f682e
devilkiller-ag <ashmitgupta.official@gmail.com>
2025-04-22 17:04:32 +0530

    setup: pg database

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,28 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
 from src.books.routes import book_router
+from src.db.main import init_db
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Lifespan context manager for FastAPI."""
+    print("Server Starting up...")
+    await init_db()
+    
+    yield 
+    
+    print("Server shutting down...")
 
 
 version = "v1"
+
 app = FastAPI(
     title="Quillist",
     description="A REST API for book review web service.",
-    version=version
+    version=version,
+    lifespan=lifespan
 )
 
 
