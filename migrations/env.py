b3ae5635fcaa99920b03e7de43101b0f40340af0
devilkiller-ag <ashmitgupta.official@gmail.com>
2025-04-25 14:32:49 +0530

    feat: add initial migration for users table with Alembic

--- migrations/env.py
+++ migrations/env.py
@@ -7,9 +7,19 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+from sqlmodel import SQLModel
+
+from src.auth.models import User
+from src.books.models import Book
+
+from src.config import Config as AppConfig
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+database_url = AppConfig.DATABASE_URL
+config.set_main_option("sqlalchemy.url", database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -20,7 +30,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
